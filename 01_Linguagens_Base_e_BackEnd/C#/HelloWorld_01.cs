3e5f92bb0bd175a7c2896ff5705799aee65ba52e
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-19 20:43:16 -0300

    feat: adiciona a pasta CSharp e  apenas o arquivo de codigo HelloWorld_01.cs

---
+++ 01_Linguagens_Base_e_BackEnd/C#/HelloWorld_01.cs
@@ -0,0 +1,13 @@
+using System;// Importa o namespace base do sistema Microsoft
+
+// Estrutura obrigatória de Classe (nomeada em Pascal Case)
+
+class HelloWorld_01 {
+
+    // Método/Função principal obrigatória que o interpretador .NET procura para rodar
+    static void Main() {
+        
+        // Imprime a mensagem "Hello World!" no console
+        Console.WriteLine("Hello World!");
+    }
+}
