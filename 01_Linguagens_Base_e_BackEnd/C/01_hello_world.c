824db7c428d6a97d3943cf47310f6f5c58bd7ef5
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-19 19:57:08 -0300

    feat: adicioana pasata C e o código 01_hello_world.c

---
+++ 01_Linguagens_Base_e_BackEnd/C/01_hello_world.c
@@ -0,0 +1,10 @@
+// Importa a biblioteca padrão de entrada e saída de dados
+#include <stdio.h>
+
+int main () {
+    // Imprime a mensagem "Hello, World!" na tela
+    printf("Hello, World!\n");
+    
+    // Retorna 0 para indicar que o programa terminou com sucesso
+    return 0;
+}
