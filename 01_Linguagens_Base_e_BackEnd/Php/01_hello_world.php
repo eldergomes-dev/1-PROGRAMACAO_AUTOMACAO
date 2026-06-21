72bacfd95071a5c566c3f32860f46148eff4d2fb
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-21 14:51:13 -0300

    feat: adicionando pasta e arquivo 01_hello_world.php

---
+++ 01_Linguagens_Base_e_BackEnd/Php/01_hello_world.php
@@ -0,0 +1,9 @@
+
+<?php
+// Em PHP, declaramos variáveis com '$' seguidas do padrão Snake Case
+// O construtor de linguagem echo joga o conteúdo da variável diretamente na tela
+// os comentários devem sempre estar dentro do escopo php, ou seja, entre as tags de abertura e fechamento
+
+    echo "Hello World!";
+
+?>
