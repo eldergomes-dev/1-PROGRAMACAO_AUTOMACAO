1bebd8d6b72bb531e724c14e639a732a9458570b
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-21 14:30:32 -0300

    feat: adicionando a pasta Java e o arquivo  HelloWorld_01.java

---
+++ 01_Linguagens_Base_e_BackEnd/Java/HelloWorld_01.java
@@ -0,0 +1,11 @@
+// Todo arquivo Java exige uma classe externa com o nome exato do arquivo físico
+// public static void main.Função estática principal que serve como o motor de arranque do programa
+//System.out.println. O sistema chama a saída padrão do console para realizar a impressão da string
+
+public class HelloWorld_01 {
+
+    public static void main(String[]args) {
+        System.out.println("Hello World!");
+    }
+}
+ 
