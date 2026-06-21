23430a9f85c4dc0214527209bf92faf52b3f5677
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-21 14:19:11 -0300

    feat: adicionando pasta C++ e arquivo 01_hello_world.cpp

---
+++ 01_Linguagens_Base_e_BackEnd/C++/01_hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Biblioteca para fluxos de entrada e saída no console
+#include <string>   // Biblioteca para manipulação de strings modernas
+
+// Função inicial padrão e obrigatória para execução do programa
+
+int main() {
+
+// Usamos o fluxo de saída std::cout para enviar ao print ou a variável para a tela
+// std::endl é usado para adicionar uma nova linha e garantir que o buffer seja limpo"
+// Retorna 0 para indicar que o programa terminou com sucesso
+
+    std::cout << "Hello World" << std::endl; 
+    return 0; 
+
+
+}
