482d916d6d53afa6be20e46740d598893ec16ec3
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-24 21:45:07 -0300

    feat:adicionando mudança no texto Hello World do HTML via javascript

---
+++ 04_Web_e_FrontEnd/JavaScript_FrontEnd/01_hello_world_front.js
@@ -0,0 +1,6 @@
+// Captura o elemento do HTML através do seu ID mapeado
+
+const elementoMensagem = document.getElementById("mensagem_hello_world");
+
+//Altera a propriedade de texto interno do elemento inserindo a nossa mensagem
+elementoMensagem.textContent = "Hello World - Alterado via JavaScript";
