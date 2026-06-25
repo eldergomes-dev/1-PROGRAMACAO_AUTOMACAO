bf0ec5e74d13da7d966b8555e1e6727e971009cd
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-24 21:58:14 -0300

    feat: adicionando e atualizando exercicios de hello world da stack

--- 02_Google_Workspace_e_Automacao/automacao_Planilhas/01_Planilha_automacao_hello_world.js
+++ 02_Google_Workspace_e_Automacao/automacao_Planilhas/01_Planilha_automacao_hello_world.js
@@ -3,3 +3,4 @@ function helloWorld() {
     // localiza a aba ativa e joga o texto na célula A1 da planilha
     SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getRange("A1").setValue("Hello World");
 }
+
--- 03_Banco_de_Dados_e_BI/MySQL/01_hello_world.sql
+++ 03_Banco_de_Dados_e_BI/MySQL/01_hello_world.sql
@@ -1,8 +1,1 @@
-
--- O comando seleciona a string "Hello World" e a exibe com o alias (nome) de coluna "mensagem_hello_world". 
-
 SELECT "Hello World" AS mensagem_hello_world;
-
-
-
-
