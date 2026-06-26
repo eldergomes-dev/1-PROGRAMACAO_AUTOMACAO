ed9724d1a64c854c9d441fd550cdf9810d479b27
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-26 13:07:29 -0300

    feat: aula 2 - gerenciamento de variaveis em javascript backend

---
+++ 01_Linguagens_Base_e_BackEnd/JavaScript_BackEnd/02_variaveis.js
@@ -0,0 +1,9 @@
+// Declaramos uma constante e uma variável usando a semântica Camel Case
+
+const identificadorCliente = "Paulo_2026";
+let saldoCarteiraDigital = 1500.50;
+
+// Exibimos as informações combinando texto estruturado e variáveis no console
+
+console.log("ID do Cliente Ativo:", identificadorCliente);
+console.log("Saldo Atual da Carteira Digital: R$", saldoCarteiraDigital.toFixed(2));
