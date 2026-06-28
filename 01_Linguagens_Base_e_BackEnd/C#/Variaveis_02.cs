a65af2675e32636eafff46b3436ecbdea38bac99
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-28 18:13:59 -0300

    feat: adicionando projetos HelloWorld_01 e Variaveis_02 organizados

---
+++ 01_Linguagens_Base_e_BackEnd/C#/Variaveis_02.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Variaveis_02 {
+   
+   static void Main() {
+
+// Declarando variáveis
+    string nomeDoAdministrador = "João da Silva";
+    int totalProjetosAtivos = 5;
+
+    Console.WriteLine("Nome do Administrador: " + nomeDoAdministrador);
+    Console.WriteLine("Total de Projetos Ativos: " + totalProjetosAtivos);
+    
+    }
+}
---
+++ 01_Linguagens_Base_e_BackEnd/C#/Variaveis_02/Variaveis_02.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nomeDoAdministrador = "João da Silva";
+int totalProjetosAtivos = 5;
+
+Console.WriteLine("Nome do Administrador: " + nomeDoAdministrador);
+Console.WriteLine("Total de Projetos Ativos: " + totalProjetosAtivos);
