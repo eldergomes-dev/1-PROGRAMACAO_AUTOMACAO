4e7b035b421f816b6764d726648327637b53824d
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-29 14:10:54 -0300

    feat: adicionando variáveis no java

---
+++ 01_Linguagens_Base_e_BackEnd/Java/Variaveis_02.java
@@ -0,0 +1,14 @@
+
+public class Variaveis_02 {
+
+    public static void main(String[] args) {
+        // Alocamos espaço em memória RAM respeitando os tipos e o padrão Camel Case
+        String nomeDoAnalista = "João da Silva";
+        boolean sistemaOperacionalAtivo = true;
+
+        // Exibindo os valores das variáveis
+        System.out.println("Analista de automação: " + nomeDoAnalista);
+        System.out.println("Status de conexão do sistema: " + sistemaOperacionalAtivo);
+        
+    }
+}
