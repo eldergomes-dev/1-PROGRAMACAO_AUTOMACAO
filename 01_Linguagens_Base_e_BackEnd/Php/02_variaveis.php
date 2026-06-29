501e8e3e92817954b5bce40d371f0c578cacd6d4
eldergomes-dev <gomesdasilvasantoselder7@gmail.com>
2026-06-29 15:09:00 -0300

    feat: Variaveis em PHP BackEnd

---
+++ 01_Linguagens_Base_e_BackEnd/Php/02_variaveis.php
@@ -0,0 +1,20 @@
+
+<?php
+
+// PHP Back-End - Variáveis
+// Em PHP, as variáveis nascem com o caractere '$' adotando a semântica Snake Case
+
+$nome_do_diretor = "José da Silva";
+$taxa_conversao_automacao = 94.8;
+
+// Usamos o comando echo combinado com o caractere de concatenação (ponto) para exibir na tela
+
+echo "Diretor de Operações: " . $nome_do_diretor . "\n";
+echo "Eficiência de processos: " . $taxa_conversao_automacao . "%\n";
+
+?>
+
+
+
+
+
